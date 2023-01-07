2ced7e81d9cd58fb9d8565123c172ad213fa67e0
Roman Mendaliev <reacheight8@gmail.com>
2023-01-07 19:45:16 +0300

    Make app function component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,84 +1,56 @@
-import React from 'react'
-import Authentication from '../../util/Authentication/Authentication'
+import React, {useEffect, useState} from 'react'
 
 import './App.css'
 import Profile from "../Profile/Profile";
 import {createTheme, ThemeProvider} from "@mui/material";
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.Authentication = new Authentication()
+const App = () => {
+    const twitch = window.Twitch ? window.Twitch.ext : null
 
-        this.twitch = window.Twitch ? window.Twitch.ext : null
-        this.state = {
-            finishedLoading: false,
-            theme: 'light',
-            isVisible: true,
-            profileName: null,
-        }
-    }
+    const [theme, setTheme] = useState('light')
+    const [isVisible, setIsVisible] = useState(true)
+    const [profileName, setProfileName] = useState(null)
 
-    contextUpdate(context, delta) {
-        if (delta.includes('theme')) {
-            this.setState({theme: context.theme})
-        }
-    }
-
-    visibilityChanged(isVisible) {
-        this.setState({isVisible})
-    }
-
-    componentDidMount() {
-        if (this.twitch) {
-            this.twitch.onAuthorized((auth) => {
-                this.Authentication.setToken(auth.token, auth.userId)
-                this.setState({finishedLoading: true})
+    useEffect(() => {
+        if (twitch) {
+            twitch.onContext((context, delta) => {
+                if (delta.includes('theme')) {
+                    setTheme(context.theme)
+                }
             })
 
-            this.twitch.onVisibilityChanged((isVisible, _c) => {
-                this.visibilityChanged(isVisible)
+            twitch.onVisibilityChanged((isVisible, _c) => {
+                setIsVisible(isVisible)
             })
 
-            this.twitch.onContext((context, delta) => {
-                this.contextUpdate(context, delta)
-            })
-
-            this.twitch.configuration.onChanged(() => {
-                let profileName = this.twitch.configuration.broadcaster ? this.twitch.configuration.broadcaster.content : null
-                this.setState({profileName})
+            twitch.configuration.onChanged(() => {
+                let profileName = twitch.configuration.broadcaster ? twitch.configuration.broadcaster.content : null
+                setProfileName(profileName)
             })
         }
-    }
+    })
 
-    render() {
-        const theme = createTheme({
-            palette: {
-                mode: this.state.theme,
-                primary: {
-                    main: this.state.theme === 'light' ? '#232127' : '#e5e3e8',
-                    light: '#e5e3e8',
-                    dark: '#232127',
-                },
+    const muiTheme = createTheme({
+        palette: {
+            mode: theme,
+            primary: {
+                main: theme === 'light' ? '#232127' : '#e5e3e8',
+                light: '#e5e3e8',
+                dark: '#232127',
             },
-        })
+        },
+    })
 
-        if (this.state.finishedLoading && this.state.isVisible) {
-            return (
-                <div className="App">
-                    <div className={this.state.theme === 'light' ? 'App-light' : 'App-dark'}>
-                        {!this.state.profileName && <p>Please configure your MyAnimeList username for extension.</p>}
-                        {this.state.profileName && <ThemeProvider theme={theme}><Profile name={this.state.profileName} /></ThemeProvider>}
-                    </div>
-                </div>
-            )
-        } else {
-            return (
-                <div className="App">
-                    Something went wrong.
-                </div>
-            )
-        }
+    if (!isVisible) return null
 
-    }
+    return (
+        <div className="App">
+            <div className={theme === 'light' ? 'App-light' : 'App-dark'}>
+                {!profileName && <p>Please configure your MyAnimeList username for extension.</p>}
+                {profileName && <ThemeProvider theme={muiTheme}><Profile name={profileName} /></ThemeProvider>}
+            </div>
+        </div>
+    )
 }
+
+export default App
