ae5b00a656e99cf55dd18fd9a524d133e0b26dc2
Roman Mendaliev <reacheight8@gmail.com>
2023-01-10 23:20:29 +0300

    Change config ui a bit

--- src/components/ConfigPage/ConfigPage.js
+++ src/components/ConfigPage/ConfigPage.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react'
 
 import './Config.css'
+import {Button, createTheme, TextField, ThemeProvider} from "@mui/material";
 
 const ConfigPage = () => {
     const twitch = window.Twitch ? window.Twitch.ext : null
 
     const [theme, setTheme] = useState('light')
     const [profileName, setProfileName] = useState(null)
+    const [isSaved, setIsSaved] = useState(false)
 
     useEffect(() => {
         if (twitch) {
@@ -20,21 +22,31 @@ const ConfigPage = () => {
 
     const handleInputChange = e => setProfileName(e.target.value)
 
-    const onSubmit = e => {
+    const onSave = e => {
         e.preventDefault()
         twitch.configuration.set('broadcaster', '1.0', profileName)
+        setIsSaved(true)
     }
+
+    const muiTheme = createTheme({
+        palette: {
+            mode: theme,
+            primary: {
+                main: theme === 'light' ? '#232127' : '#e5e3e8',
+                light: '#232127',
+                dark: '#e5e3e8',
+            },
+        },
+    })
 
     return (
         <div className="Config">
             <div className={theme === 'light' ? 'Config-light' : 'Config-dark'}>
-                <form onSubmit={onSubmit}>
-                    <label>
-                        new Enter your MyAnimeList profile name:
-                        <input type="text" onChange={handleInputChange} />
-                    </label>
-                    <button type={'submit'}>Ok</button>
-                </form>
+                <ThemeProvider theme={muiTheme}>
+                    <TextField label={'MyAnimeList username'} variant={'outlined'} onChange={handleInputChange} />
+                    <br />
+                    <Button sx={{marginTop: "10px"}} variant={'contained'} color="success" onClick={onSave}>{isSaved ? 'Saved!' : 'Save'}</Button>
+                </ThemeProvider>
             </div>
         </div>
     )
